1f3ec7579baae6199eb99f7d808fc7ada0109e54
Daniel Alvarado Perez <97266996+danieleconomist99@users.noreply.github.com>
2025-09-27 23:22:01 -0500

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>PayJoy Reviews</h1>
+      <p>Ir al formulario: <a href="/form?store=CEN">/form?store=CEN</a></p>
+    </main>
+  );
+}
